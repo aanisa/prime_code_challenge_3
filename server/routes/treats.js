d10067e5370b352f3b9a5c5bf6f7a2ec9e93694b
Anisa Abdulkadir <anisa.ali7@gmail.com>
2017-03-27 13:41:45 -0500

    POST request complete

--- server/routes/treats.js
+++ server/routes/treats.js
@@ -42,28 +42,31 @@ router.get('/', function (req, res) {
 // POST /treats
 router.post('/', function(req, res) {
 console.log(req.body);
+var name = req.body.name;
+var description = req.body.description;
+var pic = req.body.description;
 
-  // pool.connect(function(err, client, done) {
-  //     if (err) {
-  //         console.log("Error connecting to database!");
-  //         res.send(500);
-  //         done();
-  //         return;
-  //     } else {
-  //         client.query('INSERT INTO treats (name, description, pic) VALUES ($1, $2, $3)',
-  //             [name, description, pic],
-  //             function(queryError, result) {
-  //                 done();
-  //                 if (queryError) {
-  //                     console.log('Error making query!');
-  //                     res.send(500);
-  //                 } else {
-  //                     res.send(200);
-  //                     console.log(result.rows);
-  //                   }
-  //             });
-  //     }
-  // }
+  pool.connect(function(err, client, done) {
+      if (err) {
+          console.log("Error connecting to database!");
+          res.send(500);
+          done();
+          return;
+      } else {
+          client.query('INSERT INTO treats (name, description, pic) VALUES ($1, $2, $3)',
+              [name, description, pic],
+              function(queryError, result) {
+                  done();
+                  if (queryError) {
+                      console.log('Error making query!');
+                      res.send(500);
+                  } else {
+                      res.send(200);
+                      console.log(result.rows);
+                    }
+              });
+      }
+  });
 });
 
 
